e895f086cef1a1e38dee5b43c765e8416b47cbb9
Zile <yaozl2000@163.com>
2025-07-07 11:28:39 -0700

    Finished lock under reliable network

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,7 +1,10 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"time"
+
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -9,7 +12,9 @@ type Lock struct {
 	// the specific Clerk type of ck but promises that ck supports
 	// Put and Get.  The tester passes the clerk in when calling
 	// MakeLock().
-	ck kvtest.IKVClerk
+	ck        kvtest.IKVClerk
+	lockKey   string
+	lockValue string
 	// You may add code here
 }
 
@@ -19,15 +24,49 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
+	lk := &Lock{ck: ck, lockKey: l, lockValue: kvtest.RandValue(8)}
 	// You may add code here
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	for {
+		value, version, getErr := lk.ck.Get(lk.lockKey)
+		// try acquire the lock if there's no lock or the lock already be released
+		if getErr == rpc.ErrNoKey || value == "" {
+			for {
+				putErr := lk.ck.Put(lk.lockKey, lk.lockValue, version)
+				if putErr == rpc.OK {
+					return
+				}
+				if putErr == rpc.ErrVersion {
+					break
+				}
+			}
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	for {
+		value, version, getErr := lk.ck.Get(lk.lockKey)
+		// if lock does not exist or already released or it is not my lock, return
+		if getErr == rpc.ErrNoKey || value == "" || value != lk.lockValue {
+			return
+		}
+		for {
+			// try to release the lock
+			putErr := lk.ck.Put(lk.lockKey, "", version)
+			if putErr == rpc.OK {
+				return
+			}
+			if putErr == rpc.ErrVersion {
+				break
+			}
+		}
+		time.Sleep(10 * time.Millisecond)
+	}
 }
